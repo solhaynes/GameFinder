47496252753592d8ded4ee8b6fbd39ebb9e6b69b
Solomon Haynes <solomonhaynes22@gmail.com>
2024-06-19 19:14:00 -0400

    Added connection string to Project.cs

--- GameFinder.WebApi/Program.cs
+++ GameFinder.WebApi/Program.cs
@@ -1,6 +1,13 @@
+using GameFinder.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Add connection string and DbContext setup
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
