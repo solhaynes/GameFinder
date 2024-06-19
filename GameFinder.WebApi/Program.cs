1178d0356fd5102ee551a1290eafae4915baa0fd
Solomon Haynes <solomonhaynes22@gmail.com>
2024-06-19 19:32:51 -0400

    created genre branch

--- GameFinder.WebApi/Program.cs
+++ GameFinder.WebApi/Program.cs
@@ -11,4 +11,22 @@ builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlSer
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExpl
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
